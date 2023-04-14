7f889f161134791e896d67b41894885aeae2eb66
Roz <san7yx@gmail.com>
2023-04-14 00:44:35 +0000

    crossOrigin

--- pages/index.tsx
+++ pages/index.tsx
@@ -83,7 +83,11 @@ const Home: NextPage = () => {
         <title>Job Writer</title>
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
           rel="stylesheet"
