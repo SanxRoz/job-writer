c3b405a91dd31559a994ca59a3a6a1e19fb9844d
Roz <san7yx@gmail.com>
2023-04-26 23:34:19 +0000

    Update OpenAIStream.ts

--- utils/OpenAIStream.ts
+++ utils/OpenAIStream.ts
@@ -43,6 +43,7 @@ export async function OpenAIStream(payload: OpenAIStreamPayload) {
       function onParse(event: ParsedEvent | ReconnectInterval) {
         if (event.type === "event") {
           const data = event.data;
+          console.log(data); // add this line
           // https://beta.openai.com/docs/api-reference/completions/create#completions/create-stream
           if (data === "[DONE]") {
             controller.close();
